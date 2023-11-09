992f20eea5eebec4de7eb44bbde82aa96259ffb4
Vadim Khimilonov <regester_account@mail.ru>
2023-11-09 06:29:13 +0300

    encapsulation new approach testing - fix

--- 01-Core/1-variables/src/Test.kt
+++ 01-Core/1-variables/src/Test.kt
@@ -1,1 +1,24 @@
-var x = 1
+import java.util.*
+
+fun main(args: Array<String>) {
+    val reader = Scanner(System.`in`)
+    print("Введите два числа: ")
+    val first = reader.nextDouble()
+    val second = reader.nextDouble()
+
+    print("Введите оператор (+, -, *, /): ")
+    val operator = reader.next()[0]
+
+    val result: Double = when (operator) {
+        '+' -> first + second
+        '-' -> first - second
+        '*' -> first * second
+        '/' -> first / second
+        else -> {
+            System.out.printf("Ошибка! Оператор не верен")
+            return
+        }
+    }
+
+    System.out.printf("%.1f %c %.1f = %.1f", first, operator, second, result)
+}
