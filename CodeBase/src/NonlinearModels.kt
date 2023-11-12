85813e623f604c6af5412352d83637b287458b16
Vadim Khimilonov <regester_account@mail.ru>
2023-11-12 10:06:34 +0300

    added NonlinearModels functions

---
+++ CodeBase/src/NonlinearModels.kt
@@ -0,0 +1,68 @@
+import kotlin.math.pow
+
+fun main() {
+    val engine = Engine()
+    engine.start()
+    engine.accelerate(50.0)
+    engine.brake()
+    engine.stop()
+}
+
+// пример кода на Kotlin, который демонстрирует использование нелинейных моделей
+// для управления двигателем автомобиля:
+
+class Engine {
+    private var rpm = 0.0
+    private var speed = 0.0
+
+    fun start() {
+        println("Двигатель запущен")
+    }
+
+    fun stop() {
+        println("Двигатель остановлен")
+    }
+
+    fun accelerate(targetSpeed: Double) {
+        while (speed < targetSpeed) {
+            rpm += 100.0
+            speed = rpm.pow(2.0) / 100000.0
+            println("Ускорение: $speed км/ч")
+        }
+    }
+
+    fun brake() {
+        while (speed > 0.0) {
+            rpm -= 100.0
+            speed = rpm.pow(2.0) / 100000.0
+            println("Торможение: $speed км/ч")
+        }
+    }
+}
+
+// В данном случае мы создали класс Engine, который содержит методы для запуска и остановки
+// двигателя, а также для ускорения и торможения. В методах accelerate и brake мы используем
+// нелинейную модель для расчета скорости автомобиля на основе оборотов двигателя.
+// Это позволяет более точно описать работу двигателя в различных режимах и создавать
+// более эффективные системы управления двигателем.
+
+// пример, где используем нелинейную модель для вычисления оптимального положения дроссельной
+// заслонки и частоты вращения двигателя, которые обеспечивают максимальную эффективность работы двигателя.
+// Мы также используем функцию pow из стандартной библиотеки Kotlin для вычисления степеней переменных.
+fun engineControl(throttle: Double, rpm: Double): Double {
+    val a = 0.0001
+    val b = 0.001
+    val c = 0.01
+    val d = 0.1
+    val e = 1.0
+    val f = 10.0
+    val g = 100.0
+    val h = 1000.0
+
+    val x = throttle
+    val y = rpm
+
+    val z = a * x.pow(7) + b * x.pow(6) + c * x.pow(5) + d * x.pow(4) + e * x.pow(3) + f * x.pow(2) + g * x + h
+
+    return z / y
+}
